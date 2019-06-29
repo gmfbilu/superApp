62e8cb838ce6cc94bcd7af27678633524a78a5b8
gmfbilu <gmfbilu@hotmail.com>
2019-06-29 10:52:06 +0800

    add fevelop_test 2

--- app/src/main/java/org/gmfbilu/superapp/Test.java
+++ app/src/main/java/org/gmfbilu/superapp/Test.java
@@ -7,5 +7,11 @@ public class Test {
         Integer integer1 = Integer.valueOf(10);
         System.out.println(integer.equals(integer1));
         System.out.println(integer == integer1);
+
+        System.out.println("this is develop_test branch");
+        System.out.println("this is master branch");
+        System.out.println("this is develop_test branch 1");
+        System.out.println("this is master branch 1");
+        System.out.println("this is develop_test branch 2");
     }
 }
