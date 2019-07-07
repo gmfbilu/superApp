1a73420a934e2d7d150282ab502d27cbeb5f0202
gmfbilu <gmfbilu@hotmail.com>
2019-07-07 23:07:33 +0800

    master branch test merge commit log2

--- app/src/main/java/org/gmfbilu/superapp/Test.java
+++ app/src/main/java/org/gmfbilu/superapp/Test.java
@@ -4,5 +4,6 @@ public class Test {
 
     public static void main(String[] arge) {
         System.out.println("this is master test merge commit log1");
+        System.out.println("this is master test merge commit log2");
     }
 }
