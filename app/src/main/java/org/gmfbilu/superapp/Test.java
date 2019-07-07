16d64f85e3847f58c1befe71c54c366e7a5c61f5
gmfbilu <gmfbilu@hotmail.com>
2019-07-07 23:06:26 +0800

    master branch test merge log1

--- app/src/main/java/org/gmfbilu/superapp/Test.java
+++ app/src/main/java/org/gmfbilu/superapp/Test.java
@@ -3,22 +3,6 @@ package org.gmfbilu.superapp;
 public class Test {
 
     public static void main(String[] arge) {
-        Integer integer = Integer.valueOf(10);
-        Integer integer1 = Integer.valueOf(10);
-        System.out.println(integer.equals(integer1));
-        System.out.println(integer == integer1);
-        System.out.println("master stash1");
-        System.out.println("this is develop_test branch");
-        System.out.println("this is master branch");
-        System.out.println("this is develop_test branch 1");
-        System.out.println("this is master branch 1");
-        System.out.println("this is develop_test branch 2");
-        System.out.println("this is develop_test branch 3");
-        System.out.println("this is master branch 2");
-        System.out.println("this is master branch 3");
-        System.out.println("this is master branch 4");
-        System.out.println("this is develop_test branch 4");
-        System.out.println("this is master branch 5");
-        System.out.println("this is master branch 6");
+        System.out.println("this is master test merge commit log1");
     }
 }
